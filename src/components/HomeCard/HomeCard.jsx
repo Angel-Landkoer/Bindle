3602d48d4a0217cd5333ee9f8de1cdfa5b7f698e
angelLandkoer <angel.landkoer09@gmail.com>
2023-07-13 07:39:44 -0500

    Agregando estilos

--- src/components/HomeCard/HomeCard.jsx
+++ src/components/HomeCard/HomeCard.jsx
@@ -1,8 +1,8 @@
 /* eslint-disable react/prop-types */
 export function HomeCard({ i, data }) {
   return (
-    <section className="mobile:flex mobile:flex-col mobile:justify-center mobile:items-start">
-      <figure className="mobile:w-52 mobile:h-36 mobile:mb-2">
+    <section className="mobile:flex mobile:flex-col mobile:justify-center mobile:items-start desktop:mt-5">
+      <figure className="mobile:w-52 mobile:h-36 mobile:mb-2 desktop:w-[288px] desktop:h-[210px]">
         <img
           className="mobile:w-full mobile:h-full rounded-xl"
           src={`${
@@ -12,16 +12,16 @@ export function HomeCard({ i, data }) {
           alt={`Image to the ${data.title} version-${i}`}
         />
       </figure>
-      <span className="mb-2 mobile:text-xs mobile:font-medium mobile:text-darkBlue">
+      <span className="mb-2 mobile:text-xs mobile:font-medium mobile:text-darkBlue desktop:text-sm">
         ⭐{data.stars}
       </span>
-      <span className="mb-2 mobile:text-lg mobile:font-medium mobile:text-charcoal">
+      <span className="mb-2 mobile:text-lg mobile:font-medium mobile:text-charcoal desktop:text-xl desktop:font-bold">
         {data.title}
       </span>
-      <span className="mb-2 mobile:text-sm mobile:font-medium mobile:text-darkBlue">
+      <span className="mb-2 mobile:text-sm mobile:font-medium mobile:text-darkBlue desktop:text-lg">
         {data.title}|{data.city}
       </span>
-      <span className="mb-2 mobile:text-lg mobile:font-bold decoration-solid underline mobile:text-blue">
+      <span className="mb-2 mobile:text-lg mobile:font-bold decoration-solid underline mobile:text-blue desktop:text-2xl">
         $ {data.price}/ night
       </span>
     </section>
