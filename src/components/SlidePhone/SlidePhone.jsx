839fc0d848244e2311531fe8d0b99ab2c1de156c
angelLandkoer <angel.landkoer09@gmail.com>
2023-07-13 07:36:08 -0500

    Agregando y cambiando estilos

--- src/components/SlidePhone/SlidePhone.jsx
+++ src/components/SlidePhone/SlidePhone.jsx
@@ -1,21 +1,31 @@
 export function SlidePhone() {
   return (
-    <section className="mobile:grid mobile:grid-cols-3 mobile:grid-rows-2 mobile:w-full mobile:h-full mobile:my-10 mobile:py-3 mobile:overflow-x-scroll">
+    <section className="mobile:grid mobile:grid-cols-3 mobile:grid-rows-2 mobile:w-full mobile:h-full mobile:my-10 mobile:py-3 mobile:overflow-x-scroll laptop:gap-y-3 laptop:overflow-visible">
       <section className="mobile:relative mobile:col-start-1 mobile:row-start-1 mobile:row-span-2 mobile:flex mobile:flex-col mobile:justify-center mobile:items-center">
-        <figure className="mobile:w-56 mobile:h-[170px]">
+        <figure className="mobile:w-56 mobile:h-[170px] laptop:w-[363.823px] laptop:h-[277.398px] desktop:w-[522px] desktop:h-[398px]">
           <img
             src="https://raw.githubusercontent.com/Angel-Landkoer/The-pictures/d86d5f1f242389263f872686cce3b11b6b62c837/bindle/imgStatic/sectionTerAndGall/Rectangle222.svg"
             alt=""
-            className="mobile:w-full mobile:h-full mobile:rounded-lg" />
+            className="mobile:w-full mobile:h-full mobile:rounded-lg desktop:rounded-2xl"
+          />
         </figure>
-        <section className="mobile:absolute mobile:bottom-[-35px] mobile:flex mobile:flex-col mobile:justify-center mobile:items-center mobile:w-44 mobile:h-20 mobile:px-2  mobile:rounded-lg mobile:bg-white/30 mobile:backdrop-blur-lg">
-          <figure className="mobile:absolute mobile:top-[-14px] mobile:w-7 mobile:h-7 ">
+        <section
+          className="
+        mobile:absolute mobile:bottom-[-55px] mobile:flex mobile:flex-col mobile:justify-center mobile:items-center mobile:w-44 mobile:h-20 mobile:px-2 mobile:rounded-lg mobile:bg-white/30 mobile:backdrop-blur-lg
+        sm:px-2
+        laptop:w-[226.518px] laptop:h-[117.79px] laptop:bottom-[-15px]
+        xl:bottom-[-25px]
+        desktop:bottom-[-15px] desktop:w-[325px] desktop:h-[169px] desktop:rounded-2xl
+        "
+        >
+          <figure className="mobile:absolute mobile:top-[-14px] mobile:w-7 mobile:h-7 sm:top-[-17px]">
             <img
               src="https://github.com/Angel-Landkoer/The-pictures/blob/master/bindle/icons/iconTerm&Gall.png?raw=true"
               alt=""
-              className="mobile:w-full mobile:h-full mobile:rounded-full" />
+              className="mobile:w-full mobile:h-full mobile:rounded-full"
+            />
           </figure>
-          <p className="mobile:text-[10px] mobile:leading-3 mobile:font-normal mobile:text-center mobile:text-charcoal">
+          <p className="mobile:text-[10px] mobile:leading-3 mobile:font-normal mobile:text-center mobile:text-charcoal sm:text-xs desktop:p-3 desktop:text-lg desktop:font-medium">
             I’ve spent a wonderful time here. Will come back for sure next year
             together with my family and friends.
           </p>
@@ -23,39 +33,51 @@ export function SlidePhone() {
       </section>
 
       <section className="mobile:relative mobile:col-start-2 mobile:row-start-1 mobile:row-span-2 mobile:flex mobile:flex-col mobile:justify-center mobile:items-center">
-        <figure className="mobile:w-[139px] mobile:h-[170px]">
+        <figure className="mobile:w-[139px] mobile:h-[170px] laptop:w-[226.518px] laptop:h-[277.398px] desktop:w-[325px] desktop:h-[398px]">
           <img
             src="https://raw.githubusercontent.com/Angel-Landkoer/The-pictures/89aa843d8c73362d1b0f8859b6068c1c1861877f/bindle/imgStatic/sectionTerAndGall/Rectangle223.svg"
             alt=""
-            className="mobile:w-full mobile:h-full mobile:rounded-lg" />
+            className="mobile:w-full mobile:h-full mobile:rounded-lg"
+          />
         </figure>
-        <section className="mobile:absolute mobile:bottom-[-35px] mobile:flex mobile:flex-col mobile:justify-center mobile:items-center mobile:w-28 mobile:h-24 mobile:px-2  mobile:rounded-lg mobile:bg-white/30 mobile:backdrop-blur-lg">
-          <figure className="mobile:absolute mobile:top-[-14px] mobile:w-7 mobile:h-7 ">
+        <section
+          className="
+          mobile:absolute mobile:bottom-[-55px] mobile:flex mobile:flex-col mobile:justify-center mobile:items-center mobile:w-28 mobile:h-24 mobile:px-2 mobile:rounded-lg mobile:bg-white/30 mobile:backdrop-blur-lg 
+          sm:px-2 
+          laptop:w-[165.184px] laptop:h-[117.79px] laptop:bottom-[-20px]
+          desktop:bottom-[-15px] desktop:w-[237px] desktop:h-[169px] desktop:rounded-2xl
+          "
+        >
+          <figure className="mobile:absolute mobile:top-[-14px] mobile:w-7 mobile:h-7 sm:top-[-17px]">
             <img
               src="https://github.com/Angel-Landkoer/The-pictures/blob/master/bindle/icons/iconTerm&Gall.png?raw=true"
               alt=""
-              className="mobile:w-full mobile:h-full mobile:rounded-full" />
+              className="mobile:w-full mobile:h-full mobile:rounded-full"
+            />
           </figure>
-          <p className="mobile:text-[10px] mobile:leading-3 mobile:font-normal mobile:text-center mobile:text-charcoal">
+          <p className="mobile:text-[10px] mobile:leading-3 mobile:font-normal mobile:text-center mobile:text-charcoal sm:text-xs desktop:p-3 desktop:text-lg desktop:font-medium">
             I absolutely love the beach and water here. Everything was
             wonderful.
           </p>
         </section>
       </section>
-      <section className="mobile:w-[168px] mobile:h-20">
+
+      <section className="mobile:w-[168px] mobile:h-20 laptop:self-end laptop:w-[274.61px] laptop:h-[131.729px] desktop:w-[394px] desktop:h-[189px]">
         <figure className="mobile:w-full mobile:h-full">
           <img
             src="https://raw.githubusercontent.com/Angel-Landkoer/The-pictures/89aa843d8c73362d1b0f8859b6068c1c1861877f/bindle/imgStatic/sectionTerAndGall/Rectangle224.svg"
             alt=""
-            className="mobile:w-full mobile:h-full" />
+            className="mobile:w-full mobile:h-full"
+          />
         </figure>
       </section>
-      <section className="mobile:w-[168px] mobile:h-20">
+      <section className="mobile:w-[168px] mobile:h-20 laptop:w-[274.61px] laptop:h-[131.729px] desktop:w-[394px] desktop:h-[189px]">
         <figure className="mobile:w-full mobile:h-full">
           <img
             src="https://raw.githubusercontent.com/Angel-Landkoer/The-pictures/89aa843d8c73362d1b0f8859b6068c1c1861877f/bindle/imgStatic/sectionTerAndGall/Rectangle225.svg"
             alt=""
-            className="mobile:w-full mobile:h-full" />
+            className="mobile:w-full mobile:h-full"
+          />
         </figure>
       </section>
     </section>
