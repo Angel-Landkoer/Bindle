0371fff579d29d8ca4d434a1b374ea0da9cd78a4
angelLandkoer <angel.landkoer09@gmail.com>
2023-07-13 07:37:52 -0500

    Añadiendo estilos

--- src/components/SlideVideo/SlideVideo.jsx
+++ src/components/SlideVideo/SlideVideo.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 export function SlideVideo({ data, funcMap }) {
   return (
-    <section className="mobile:flex mobile:items-center mobile:gap-x-5 mobile:h-full mobile:w-full mobile:py-3 mobile:my-10 mobile:overflow-x-scroll">
+    <section className="mobile:flex mobile:items-center mobile:gap-x-5 mobile:h-full mobile:w-full mobile:py-3 mobile:my-10 mobile:overflow-x-scroll laptop:overflow-visible laptop:justify-evenly laptop:gap-0 laptop:my-4">
       {data.map(funcMap)}
     </section>
   );
